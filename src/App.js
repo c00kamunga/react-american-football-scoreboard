7ffc65f987fb5ce702a7562a50aed652cdc020b2
c00kamunga <tsothalanti2@yahoo.com>
2020-02-26 16:53:15 -0800

    transfered corresponding code to Home.js from App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import React, {useState} from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
+import Home from './Home'
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export default function Home() {
+    const [homeScore, setHomeScore] = useState(0);
     return (
-        <div>
+        <div className="container">
+      <section className="scoreboard">
+        <div className="topRow">
+          <div className="home">
+            <h2 className="home__name">Lions</h2>
+
             
+
+  <div className="home__score">{homeScore}</div>
+          </div>
+          <div className="timer">00:03</div>
         </div>
+        <BottomRow />
+      </section>
+      <section className="buttons">
+        <div className="homeButtons">
+          <button onClick = {() =>setHomeScore (homeScore + 7)} className="homeButtons__touchdown">Home Touchdown</button>
+          <button onClick = {() =>setHomeScore (homeScore + 3)} className="homeButtons__fieldGoal">Home Field Goal</button>
+        </div>
+      </section>
+    </div>
     )
 }
